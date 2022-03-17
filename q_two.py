491584b037a3ec2984be652b9b244c3d653f3f96
cpk <cpk0709@gmail.com>
2022-03-17 09:18:27 +0900

    secing

--- q_two.py
+++ q_two.py
@@ -71,7 +71,7 @@ def longestPalindromeAnswer(s:str) -> str:
         return s
 
     result = ''
-    # 술라이딩 윈도우 우측으로 이동
+
     for i in range(len(s)-1):
         result = max(result,
                      expand(i, i+1),
